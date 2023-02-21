a1d4f8219579bec5828a6e1a444ef023b99f1b2c
Davinder Singh <davinder.singh@sourcefuse.com>
2023-02-21 13:24:58 +0530

    Create a simple LB4 REST application with CRUD for users. Use in-memory connector.

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,86 @@
+import {Entity, model, property} from '@loopback/repository';
+
+export enum Role {
+  SuperAdmin = 'SuperAdmin',
+  Admin = 'Admin',
+  Subscriber = 'Subscriber',
+}
+
+@model()
+export class User extends Entity {
+  @property({
+    id: true,
+    require: true,
+    type: 'number',
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+    jsonSchema: {
+      minLength: 3,
+    },
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    jsonSchema: {
+      minLength: 3,
+    },
+  })
+  middleName?: string;
+
+  @property({
+    type: 'string',
+    jsonSchema: {
+      minLength: 3,
+    },
+  })
+  lastName?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    jsonSchema: {
+      minLength: 3,
+      format: 'email',
+    },
+  })
+  email: string;
+
+  @property({
+    type: 'number',
+  })
+  phone?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+    jsonSchema: {
+      enum: Object.values(Role),
+    },
+  })
+  role: Role;
+
+  @property({
+    type: 'date',
+    defaultFn: 'now',
+  })
+  createdOn?: Date;
+
+  @property({
+    type: 'date',
+    defaultFn: 'now',
+  })
+  modifiedOn?: Date;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {}
+
+export type UserWithRelations = User & UserRelations;
